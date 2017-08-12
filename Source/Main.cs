0aad95e4a6ffb1e1e95228cbdc46e11038cdb5ed
Travis Offtermatt <travis.offtermatt@gmail.com>
2017-08-12 11:27:00 -0600

    working on schedule fix

--- Source/Main.cs
+++ Source/Main.cs
@@ -26,6 +26,20 @@ namespace ForceDoJob
     [HarmonyPatch(typeof(FloatMenuMakerMap), "ChoicesAtFor")]
     static class Patch_FloatMenuMakerMap_ChoicesAtFor
     {
+        private static FieldInfo pfi;
+        private static bool useWorkTab;
+
+        private static void SetPriority(Pawn_WorkSettings workSettings, WorkTypeDef workTypeDef, int priority)
+        {
+            ((DefMap<WorkTypeDef, int>)pfi.GetValue(workSettings))[workTypeDef] = priority;
+        }
+
+        internal static Action<Pawn, Def, int> VanillaPrioritySetter = null;
+        internal static Action<Pawn, Def, int, int> WorkTabPrioritySetter = null;
+        internal static Func<Pawn, Def, int> VanillaPriorityGetter = null;
+        internal static Func<Pawn, Def, int, int> WorkTabPriorityGetter = null;
+        internal static Func<Pawn, Def, bool> DisabledGetter = null;
+
         static Patch_FloatMenuMakerMap_ChoicesAtFor()
         {
             pfi = typeof(Pawn_WorkSettings).GetField("priorities", BindingFlags.Instance | BindingFlags.NonPublic);
@@ -39,37 +53,26 @@ namespace ForceDoJob
                     // access worktab to force an error
                     var test = MainTabWindow_WorkTab.Instance;
 
-                    PrioritySetter = (pawn, def, priority) => pawn.SetPriority(def as WorkGiverDef, priority, null);
-                    PriorityGetter = (pawn, def) => pawn.GetPriority(def as WorkGiverDef, -1);
+                    WorkTabPrioritySetter = (pawn, def, priority, hour) => pawn.SetPriority(def as WorkGiverDef, priority, hour);
+                    WorkTabPriorityGetter = (pawn, def, hour) => pawn.GetPriority(def as WorkGiverDef, hour);
                     DisabledGetter = (pawn, def) => pawn.story.WorkTypeIsDisabled((def as WorkGiverDef).workType);
                 }))();
 
-                Log.Message("ForceDoJob: WorkTab detected, using compatibility mode.");
                 useWorkTab = true;
+
+                Log.Warning("ForceDoJob: WorkTab detected, using compatibility mode.");
             }
             catch (TypeLoadException)
             {
-                PrioritySetter = (pawn, def, priority) => SetPriority(pawn.workSettings, def as WorkTypeDef, priority);
-                PriorityGetter = (pawn, def) => pawn.workSettings.GetPriority(def as WorkTypeDef);
+                VanillaPrioritySetter = (pawn, def, priority) => SetPriority(pawn.workSettings, def as WorkTypeDef, priority);
+                VanillaPriorityGetter = (pawn, def) => pawn.workSettings.GetPriority(def as WorkTypeDef);
                 DisabledGetter = (pawn, def) => pawn.story.WorkTypeIsDisabled(def as WorkTypeDef);
 
-#if DEBUG
-                Log.Message("ForceDoJob: using vanilla priority getter/setter.");
-#endif
+                useWorkTab = false;
+
+                Log.Warning("ForceDoJob: using vanilla priority getter/setter.");
             }
         }
-
-        private static FieldInfo pfi;
-        private static bool useWorkTab;
-
-        private static void SetPriority(Pawn_WorkSettings workSettings, WorkTypeDef workTypeDef, int priority)
-        {
-            ((DefMap<WorkTypeDef, int>)pfi.GetValue(workSettings))[workTypeDef] = priority;
-        }
-
-        internal static Action<Pawn, Def, int> PrioritySetter;
-        internal static Func<Pawn, Def, int> PriorityGetter;
-        internal static Func<Pawn, Def, bool> DisabledGetter;
 
         static void Prefix(Pawn pawn, ref List<Pair<Def, int>> __state)
         {
@@ -79,21 +82,40 @@ namespace ForceDoJob
             List<Def> defs = useWorkTab
                 ? DefDatabase<WorkGiverDef>.AllDefsListForReading.Cast<Def>().ToList()
                 : DefDatabase<WorkTypeDef>.AllDefsListForReading.Cast<Def>().ToList();
-
+            int hour = GenLocalDate.HourOfDay(pawn);
             foreach (var def in defs)
             {
                 if (!DisabledGetter(pawn, def))
                 {
-                    __state.Add(new Pair<Def, int>(def, PriorityGetter(pawn, def)));
-                    PrioritySetter(pawn, def, 3);
+                    if (useWorkTab)
+                    {
+                        if (PriorityManager.Get.ShowScheduler)
+                        {
+                            __state.Add(new Pair<Def, int>(def, WorkTabPriorityGetter(pawn, def, hour)));
+                            WorkTabPrioritySetter(pawn, def, 3, hour);
+                        }
+                    }
+                    else
+                    {
+                        __state.Add(new Pair<Def, int>(def, VanillaPriorityGetter(pawn, def)));
+                        VanillaPrioritySetter(pawn, def, 3);
+                    }
                 }
             }
         }
         static void Postfix(Pawn pawn, ref List<Pair<Def, int>> __state)
         {
+            int hour = GenLocalDate.HourOfDay(pawn);
             foreach (Pair<Def, int> p in __state)
             {
-                PrioritySetter(pawn, p.First, p.Second);
+                if (useWorkTab)
+                {
+                    WorkTabPrioritySetter(pawn, p.First, p.Second, hour);
+                }
+                else
+                {
+                    VanillaPrioritySetter(pawn, p.First, p.Second);
+                }
             }
             __state.Clear();
         }
